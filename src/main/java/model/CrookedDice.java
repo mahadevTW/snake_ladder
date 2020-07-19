27d23e0f80b3fd79e3bd1341327c74d2ff31c823
mahadev <mahadev.wce@gmail.com>
2020-07-19 21:02:35 +0530

    change min number for crooked dice

--- src/main/java/model/CrookedDice.java
+++ src/main/java/model/CrookedDice.java
@@ -4,7 +4,7 @@ import java.util.Random;
 
 public class CrookedDice implements Throwable {
     public int throw_() {
-        int min = 0;
+        int min = 2;
         int max = 6;
         return min + new Random().nextInt((max - min) / 2) * 2;
     }
